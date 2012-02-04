9194eb043e279bd406f829358fbb1857dfe9391a
Samuel Gélineau <gelisam@gmail.com>
2012-02-04 18:10:13 -0500

    Always build before running
    Since the building system bails out early when the application is
    up-to-date, the "does the executable exist?" check was useless.

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@ set -e
 
 APP="$(basename "$(pwd)")"
 
-if [ ! -d "$APP.app" ]; then
-  ./build.sh
-fi
+# make sure the app is up-to-date
+./build.sh
+
 if [ "$1" == "--debug" ]; then
   gdb ./$APP.app/Contents/MacOS/$APP
 else
