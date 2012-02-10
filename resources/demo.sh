0b041ab415fa7f600d25fd071ebcf3e28ae9bff2
Samuel Gélineau <gelisam@gmail.com>
2012-02-09 21:28:24 -0500

    Trailing pipe demo
    The demonstration script loaded when the script start now demonstrates
    the new pipe trimming feature.

--- resources/demo.sh
+++ resources/demo.sh
@@ -2,8 +2,9 @@
 echo "==="
 echo "Hello World!"
 
-echo "1) Move here and press F5."
+echo "1) Move here and press F5." |
+tr [a-z] [A-Z]
 echo "2) Move here and press F5."
-echo "3) Notice the difference?"
+echo "3) Notice any difference?"
 
 echo "note: look at stdout."
