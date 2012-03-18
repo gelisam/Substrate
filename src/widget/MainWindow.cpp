aa3778c9c543dc7e6b1a08fe3fc2dc26ffff16a1
Samuel Gélineau <gelisam@gmail.com>
2012-03-18 17:50:04 -0400

    Story 17: Reload and run
    Full story description:
    I can reload and run the entire project.
    
    For convenience, Substrate now does this automatically once you come
    back to the app after editing the files from outside.

--- src/widget/MainWindow.cpp
+++ src/widget/MainWindow.cpp
@@ -16,6 +16,22 @@ void MainWindow::init() {
   _menuBar->init();
   _centralWidget->init();
   _statusBar->init();
+  
+  // Automatically reload and run when coming back to the app,
+  // in case what the user was doing in that other window was
+  // editing our files.
+  QAction* reloadAction = _menuBar->runMenu()->runAction();
+  QAction* runAction    = _menuBar->runMenu()->runAction();
+  connect(this,         SIGNAL(activated()),
+          reloadAction, SLOT(act()));
+  connect(this,         SIGNAL(activated()),
+          runAction,    SLOT(trigger()));
+  
+  // Also save when leaving, otherwise the above is going to
+  // drop the user's work every time he switches applications.
+  QAction* saveAction = _menuBar->runMenu()->runAction();
+  connect(this,       SIGNAL(deactivated()),
+          saveAction, SLOT(act()));
 }
 
 
